2f5b2da4dd435f5e729b55691397e96b2ed9cc8f
Tomas Matukas <tomas.matukas@gmail.com>
2025-01-02 19:36:56 +0200

    update calc.js code

--- calculator.js
+++ calculator.js
@@ -1,53 +1,45 @@
 import { operate } from "./mathOper.js";
 
-let currentInput = "";
-let previousInput = "";
-let operator = "";
-
-export function resetCalculator() {
-  currentInput = "";
-  previousInput = "";
+let currentInput = "",
+  previousInput = "",
   operator = "";
+
+export const resetCalculator = () => {
+  currentInput = previousInput = operator = "";
   updateDisplay("");
-}
+};
 
-export function updateDisplay(value) {
-  const display = document.getElementById("display");
-  display.value = value;
-}
+export const updateDisplay = (value) => {
+  document.querySelector("#display").value = value; // Using querySelector
+};
 
-export function appendToDisplay(value) {
+export const appendToDisplay = (value) => {
   currentInput += value;
   updateDisplay(currentInput);
-}
+};
 
-export function deleteFromDisplay() {
+export const deleteFromDisplay = () => {
   currentInput = currentInput.slice(0, -1);
   updateDisplay(currentInput);
-}
+};
 
-export function setOperator(op) {
-  if (previousInput === "" && currentInput !== "") {
-    previousInput = currentInput;
+export const setOperator = (op) => {
+  if (currentInput) {
+    if (!previousInput) previousInput = currentInput;
     currentInput = "";
   }
   operator = op;
-}
+};
 
-export function calculateResult() {
-  if (previousInput !== "" && currentInput !== "") {
-    // Perform the calculation
+export const calculateResult = () => {
+  if (previousInput && currentInput) {
     const result = operate(
       Number(previousInput),
       Number(currentInput),
       operator
     );
-
-    // Display the result and update previousInput
     updateDisplay(result);
-
-    // Set the result as previousInput for further operations
     previousInput = result;
-    currentInput = ""; // Clear current input for the next value
+    currentInput = "";
   }
-}
+};
